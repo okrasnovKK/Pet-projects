7d0facb895ff025a0372de75f9da2fb75a368cab
IT-OK <o.krasnov@mail.ru>
2023-09-15 15:05:01 +0300

    web_site_django

---
+++ web_site/mysite/news/urls.py
@@ -0,0 +1,29 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    #path('', index, name='home'), ## использовался при работе с функциями отображения списка новостей
+    path('', HomeNews.as_view(), name='home'),
+    #path('category/<int:category_id>/', get_category, name='category'), ## использовался при работе с функциями отображения списка новостей
+    path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
+    # path('news/<int:news_id>/', view_news, name='view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
+    # path('news/add_news/', add_news, name='add_news')
+    path('news/add_news/', CreateNews.as_view(), name='add_news')
+]
+
