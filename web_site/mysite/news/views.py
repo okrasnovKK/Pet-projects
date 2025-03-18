3b60b9b86a59b51e892edf768e1ee7c38031c15d
okrasnovKK <145114797+okrasnovKK@users.noreply.github.com>
2025-03-18 11:23:12 +0300

    Update views.py

--- web_site/mysite/news/views.py
+++ web_site/mysite/news/views.py
@@ -11,7 +11,6 @@ class HomeNews(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    #extra_context = {'title': 'Главная'}
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -48,41 +47,8 @@ class ViewNews(DetailView):
 class CreateNews(CreateView):
     form_class = NewsForm
     template_name = 'news/add_news.html'
-    # success_url = reverse_lazy('home')
-
-
-
-# Формирование списка новостей через функцию (до ListView)
-# def index(request):
-#     news = News.objects.all()
-#     context = {
-#         'news': news,
-#         'title': 'Список новостей',
-#                }
-#     return render(request, 'news/index.html', context)
-
-
+   
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
-
-
-# def view_news(request, news_id):
-#     # news_item = News.objects.get(pk=news_id)
-#     news_item = get_object_or_404(News, pk=news_id)
-#     return render(request, 'news/view_news.html', {'news_item': news_item})
-
-
-# def add_news(request):
-#     if request.method == 'POST':
-#         form = NewsForm(request.POST)
-#         if form.is_valid():
-#             #print(form.cleaned_data)
-#             #news = News.objects.create(**form.cleaned_data) # эта команда используется при работе с формами, несвязанными с моделями
-#             news = form.save()
-#             return redirect(news)
-#     else:
-#         form = NewsForm()
-#     return render(request, 'news/add_news.html', {'form': form})
-
